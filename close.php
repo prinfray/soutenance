a3bf3304705e6e5d9d2b73d9cd86624f1f7d0418
prinfray <paul.rinfray@gmail.com>
2021-09-16 13:11:21 +0200

    abandon

--- close.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-session_start();
-$_SESSION['sess_user_id'] = "";
-$_SESSION['sess_email'] = "";
-$_SESSION['sess_name'] = "";
-if(empty($_SESSION['sess_user_id'])) header("location: index.php");
-?>
-
